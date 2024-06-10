d7113de74d956e0848537ab4f4e8c238e0e4cbba
sangampawar7005 <sangampawar7005@gmail.com>
2024-06-10 22:20:04 +0530

    Add files via upload

---
+++ src/main/java/pages/My_Account_Page.java
@@ -0,0 +1,5 @@
+package pages;
+
+public class My_Account_Page {
+
+}
---
+++ src/main/java/testbase/WebTestBase.java
@@ -0,0 +1,22 @@
+package testbase;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+public class WebTestBase {
+
+	public static Properties prop;
+	
+	public static WebDriver driver;
+
+	
+	public WebTestBase()  //constructor
+	{
+		FileInputStream fileInputStream = new FileInputStream("");
+	}
+	
+	}
+	
+
